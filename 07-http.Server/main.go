da4955abc372e120bf5edbb2ed2dad559d983d34
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-06-07 21:13:45 +0530

    crud operation completed

--- 07-http.Server/main.go
+++ 07-http.Server/main.go
@@ -6,22 +6,20 @@ import (
 	"time"
 )
 
-
 func main() {
 
-	http.HandleFunc("/home",messageHandler)
+	http.HandleFunc("/home", messageHandler)
 
-	// If we want to configure the server or if we want to customize the behavior of the server we can use Server struct present inside the http package, 
+	// If we want to configure the server or if we want to customize the behavior of the server we can use Server struct present inside the http package,
 	// and setup some functionlities like setup the port, ReadTimeout, WriteTimeout etc.
 	server := &http.Server{
-		Addr: ":8080",
-		ReadTimeout: 10*time.Second,
-		WriteTimeout: 10*time.Second,
+		Addr:           ":8080",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 	server.ListenAndServe()
 }
-
 
 func messageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("ok this is message handler")
---
+++ 09-CRUD/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Notes struct {
+	Title       string `json:"notes"`
+	Description string `json:"description"`
+}
+
+var noteStore = make(map[string]Notes)
+var i = 0
+
+func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/addnotes", addNotes).Methods("POST")           // CREATE
+	router.HandleFunc("/notes", viewNotes).Methods("GET")              // SEE ALL THE NOTES
+	router.HandleFunc("/updatenote/{id}", updateNote).Methods("PATCH") // UPDATE A NOTE
+	router.HandleFunc("/deletenote/{id}", deleteNote).Methods("DELETE") // DElETE
+	http.ListenAndServe(":8080",router)
+
+}
+
+func addNotes(w http.ResponseWriter, r *http.Request) {
+
+	var notes Notes
+	err := json.NewDecoder(r.Body).Decode(&notes)
+	if err != nil {
+		panic(err)
+	}
+	i++
+	k := strconv.Itoa(i)
+	noteStore[k] = notes
+	res, err := json.Marshal(notes)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	w.Write(res)
+
+}
+
+func viewNotes(w http.ResponseWriter, r *http.Request) {
+
+	var notes []Notes
+	for _, val := range noteStore {
+		notes = append(notes, val)
+	}
+
+	res, err := json.Marshal(notes)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	w.Write(res)
+}
+
+func updateNote(w http.ResponseWriter, r *http.Request) {
+
+	query := mux.Vars(r)
+	id := query["id"]
+
+	var notes Notes
+	err := json.NewDecoder(r.Body).Decode(&notes)
+	if err != nil {
+		panic(err)
+	}
+
+	if _, ok := noteStore[id]; ok {
+
+		delete(noteStore, id)
+		noteStore[id] = notes
+
+	} else {
+		fmt.Println("the id given does not exist")
+	}
+
+	res, err := json.Marshal(notes)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
+}
+
+func deleteNote(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	if _, ok := noteStore[id]; ok {
+		delete(noteStore, id)
+	} else {
+		fmt.Println("the id of note not present")
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusNoContent)
+
+}
