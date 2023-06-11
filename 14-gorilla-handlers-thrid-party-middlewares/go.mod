12a4e440e33fadcfce37bc571559642a378312ae
Abhinand KR <nanduttanvsabhi@gmail.com>
2023-06-11 21:08:40 +0530

    added handlers package for third party middlewares

---
+++ 14-gorilla-handlers-thrid-party-middlewares/go.mod
@@ -0,0 +1,9 @@
+module gorillahandler
+
+go 1.20
+
+require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+)
---
+++ 14-gorilla-handlers-thrid-party-middlewares/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+
+func index(w http.ResponseWriter, r *http.Request){
+	fmt.Println("the index handler")
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("the home handler")
+}
+
+func main() {
+
+	router := mux.NewRouter()
+	// here handlers.LoggingHandler is a middleware which execute items after the application handler is called. i.e after the execution of request.
+	// not that middleware can have 2 arguments. it's just that the return type must be a handler
+	router.Handle("/",handlers.LoggingHandler(os.Stdout,http.HandlerFunc(index)))
+	router.Handle("/home",handlers.LoggingHandler(os.Stdout,http.HandlerFunc(home)))
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: router,
+	}
+
+	server.ListenAndServe()
+}
